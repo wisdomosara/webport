d92f454cce6b411bbd3c4fcaca1a18dd752aa753
wisdomosara <50260832+wisdomosara@users.noreply.github.com>
2020-07-12 23:14:54 +0100

    Add files via upload

--- johndoe.js
+++ johndoe.js
@@ -1,3 +1,20 @@
+let hand = document.querySelector(".hand")
+let img = document.querySelector(".hand img")
+let clicked = false;
+window.onload = function(){
+    hand.addEventListener("click", function() {
+    clicked = !clicked
+if(clicked == true) {
+    img.src = "./images/icons8-delete-30.png"
+    console.log("open")
+}else {
+   img.src = "./images/hamburger.png"
+}
+})
+
+
+}
+
 
 AOS.init();
 let navLinks = document.querySelectorAll(".nav-item")
